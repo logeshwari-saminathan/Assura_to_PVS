4fc0c478b66a951cccc99f4f786054a8de1f01c3
Logeshwari <logeshwarisaminathan@gmail.com>
2018-05-08 16:54:00 +0530

    initial commit

--- assuraDRC_to_PVS_DRC.py
+++ assuraDRC_to_PVS_DRC.py
@@ -8,10 +8,10 @@ import string
 
 # List of token names.   This is always required
 tokens = (
-    'NAME','NUMBER',
+    'NUMBER',
     'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-    'LPAREN','RPAREN', 'MODULUS','POWER',  'GREATERTHAN', 'LESSTHAN','FLOAT','DRC','SEPNOTCH','ID','METAL1','GEOMGETNON90',
-    'ERRORINFO',   'RULMESSAGE' , 'UNDERSCORE' ,'LAYER','CHECK'
+    'LPAREN','RPAREN', 'MODULUS','POWER',  'GREATERTHAN', 'LESSTHAN','FLOAT','DRC','SEPNOTCH','ID','METAL','GEOMGETNON90',
+    'ERRORINFO','RULMESSAGE' ,'UNDERSCORE' ,'LAYER','CHECK','NBURIED' ,'NWELL','ENC',
     )
 
 # Regular expression rules for simple tokens
@@ -23,22 +23,35 @@ t_DIVIDE  = r'/'
 t_EQUALS  = r'='
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
-t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_METAL   = r'Metal\d'
+
+#t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
 t_RULMESSAGE = r'".*"'
 t_GREATERTHAN = r'\>'
 
 #reserved words
 reserved = {
-   'Metal1' : 'METAL1',
    'errorLayer': 'ERRORINFO',
    'check ': 'CHECK',
+   'enc': 'ENC',
 }
 
 # A regular expression rule with some action code
 
+def t_NWELL(t):
+    r'nwell'
+    t.value =['inLayer1']
+    return t
+
+
+def t_NBURIED(t):
+    r'nburied'
+    t.value =['inLayer2']
+    return t
+
 def t_LAYER(t):
     r'layer'
-    t.value ='layer_def'
+    t.value =['layer_def','layername']
     return t
     
 
@@ -47,6 +60,7 @@ def t_GEOMGETNON90(t):
     t.value = ['angle ', ' -ltgt 0 90']
     return t
 
+
 def t_LESSTHAN(t):
     r'\<'
     t.value = '-lt'
@@ -54,7 +68,7 @@ def t_LESSTHAN(t):
     
 def t_DRC(t):
     r'drc'
-    t.value = 'exte'
+    t.value = ['exte']
     return t
     
 def t_SEPNOTCH(t):
@@ -64,7 +78,7 @@ def t_SEPNOTCH(t):
    else:
        t.value = ""
    return t
-   
+    
 def t_ID(t):
    r'[a-zA-Z_][a-zA-Z_0-9]*'
    t.type = reserved.get(t.value,'ID')    # Check for reserved words
@@ -85,7 +99,7 @@ def t_newline(t):
     t.lexer.lineno += len(t.value)
 
 # A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
+t_ignore  = '\t \n'
 
 # Error handling rule
 def t_error(t):
@@ -96,11 +110,11 @@ def t_error(t):
 lexer = lex.lex()
 
 # Test it out  (Input)
-data = '''L998=drc(Metal1 sepNotch<0.06)
-L51265=geomGetNon90(Metal1)
-errorLayer(L998 "METAL1.SP.1.1: Metal1 to Metal1 spacing must be >= 0.06 um")
-Metal1_d=layer( 7 type(0) )
-'''
+data = '''L998=drc(Metal sepNotch<0.06)
+errorLayer(L998 "METAL.SP.1.1: Metal to Metal spacing must be >= 0.06 um")'''
+#Metal1_d=layer( 7 type(0) )
+#L91383=drc(Nburied Nwell enc<0.2)'''
+
 
 
 
@@ -108,18 +122,19 @@ Metal1_d=layer( 7 type(0) )
 #which_token = lexer.token()
 #print ('which_token = ',which_token)
 #print ("Comment:", which_token.type)
-#while True: 
-#    tok = lexer.token()
-#    if not tok: 
-#        break      # No more input
-#    print(tok)
+lexer.input(data)
+while True: 
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok)
 
 
 # Parsing rules  
 precedence = (
 ('nonassoc', 'LESSTHAN', 'GREATERTHAN'),
 ('left', 'SEPNOTCH'),
-('left','METAL1'),
+('left','METAL'),
 ('left','LPAREN','RPAREN'),
 ('left','DRC'),
     )
@@ -130,41 +145,46 @@ names = {}
 
 #[outLayer = ]drc( inLayer1 [inLayer2] check [modifiers] )
 #def p_expression(t):
-#   expression : Name? drc expression1
-#   expression1: Metal Metal? check
-#   check      : LESSTHAN | EQUALS? | GREATERTHAN? | EQUALS?
+#   expression : ID? drc expression1
+#   expression1: LPARAN Metal Metal? check RPARAN
+#   check      : LESSTHAN | EQUALS? | GREATERTHAN? | EQUALS? 
                 
     
 
                 
 
 # Parsing rules
-# From: L998=drc(Metal1 sepNotch<0.06)
-# To:exte inLayer1 inLayer1 -lt value -output region -notch not -abut lt 90; 
-# To:exte Metal1 Metal1 -lt 0.06 -output region -notch not -abut lt 90; 
-
 
 expr = []
 def print_expr():
-    for expr1 in expr:
-        print(names[expr1])
-        
+    global expr
+    for i in range(len(expr)):
+        expr1 = names[expr[i]]
+        if i ==0:
+#            print(expr1)
+            print('rule ',expr1.split(':')[0],'" {')
+            print('\tcaption',expr1)
+        else:
+            print('\t',expr)
+             
+    print('}')
+    expr = []  
 
  #From : L998=drc(Metal1 sepNotch<0.06)       
 # To : exte Metal1 Metal1 -lt 0.06 -output region -abut lt 90; 
 def p_statement_assign(t):
-    'expression :  ID EQUALS DRC LPAREN METAL1 SEPNOTCH LESSTHAN NUMBER RPAREN'
-    names[t[1]] = " ".join([t[3],t[5],t[5], t[7],str(t[8]),'-output region -abut lt 90; ' ])
+    'expression :  ID EQUALS DRC LPAREN METAL SEPNOTCH LESSTHAN NUMBER RPAREN'
+    names[t[1]] = " ".join([t[3][0],t[5],t[5], t[7],str(t[8]),'-output region -abut lt 90; ' ])
 #    names[t[1]] = t[3]
     expr.append(t[1])
 #    print(names[t[1]])
     
 
-#errorLayer(L998 "METAL1.SP.1.1: Metal1 to Metal1 spacing must be >= 0.06 um")
+#errorLayer(L998 "METAL1.SP.1.1: Metal1 to spacing must be >= 0.06 um")
 def p_statement_getRUL(t):
     'expression : ERRORINFO LPAREN ID RULMESSAGE RPAREN'
-    names[t[2]] = t[4]
-    expr.append(t[2])
+    names[t[1]] = t[4]
+    expr.append(t[1])
     expr.reverse()
     print_expr()
 
@@ -172,22 +192,30 @@ def p_statement_getRUL(t):
 #From :L51265=geomGetNon90(Metal1)
 #To:angle inLayer -ltgt 0 90 outLayer;    
 def p_statement_geonon90(t):
-    'expression : ID EQUALS GEOMGETNON90 LPAREN METAL1 RPAREN'
+    'expression : ID EQUALS GEOMGETNON90 LPAREN METAL RPAREN'
     names[t[1]] = " ".join([t[3][0],t[5], t[3][1],t[1]])
     expr.append(t[1])
     
 # From :Metal1_d=layer( 7 type(0) )
 # To:layer_def layername arbitary_number;
 def p_statement_layermap(t):
-    'expression : NAME EQUALS LAYER LPAREN NUMBER NAME LPAREN NUMBER RPAREN RPAREN '
-    names[t[1]] = " ".join([t[3][0],t[3][1], 'arbitary_number;' ])
+    'expression : ID EQUALS LAYER LPAREN NUMBER ID LPAREN NUMBER RPAREN RPAREN '
+    names[t[1]] = " ".join([t[3][1],t[3][1],'arbitary_number;' ])
     expr.append(t[1])
     
+#From : L91383=drc(Nburied Nwell enc<0.2)
+#To   : enc inLayer2 inLayer1 -lt value -output region -abut lt 90;
+def p_statement_enclose(t):
+    'expression : ID EQUALS DRC LPAREN NBURIED NWELL ENC LESSTHAN NUMBER RPAREN '
+    names[t[1]] = " ".join([t[7],t[5][0], t[6][0], t[8], t[9],'-output region -abut lt 90;'])
+    expr.append(t[1])
 
     
 import ply.yacc as yacc
 parser = yacc.yacc()
 lines = data.split('\n')
+#infl = open("Input_rule_file")
+#lines = list(infl)
 for line in lines:
     result = parser.parse(line)
 
